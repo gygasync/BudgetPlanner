a4df8e64f7cecf31321a20b4ef06533ad3eebfc7
igor-pejakovic-codaxy <igor.pejakovic@codaxy.com>
2021-10-05 15:17:04 +0200

    Add Rule and Trigger models

---
+++ BudgetPlanner.Data.Model/Rule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetPlanner.Data.Models
+{
+    public class Rule: BaseModel<long>
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Account> Parameters { get; set; }
+        public virtual ICollection<decimal> Variables { get; set; }
+        public virtual Trigger Trigger { get; set; }
+        
+    }
+}
---
+++ BudgetPlanner.Data.Model/Trigger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetPlanner.Data.Models
+{
+    public class Trigger: BaseModel<long>
+    {
+        public string Name { get; set; }
+        public string Action { get; set; }
+    }
+}
